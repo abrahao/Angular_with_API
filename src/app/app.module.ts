bec28c2d69ea38a63528b9aa147605f291ec6b61
abrahao <abrahao.eneias@dce.ufpb.br>
2021-05-09 18:11:43 -0300

    Add o modulo MatTableModule do Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { HeaderComponent } from './header/header.component';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -29,8 +28,7 @@ import { HeaderComponent } from './header/header.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule,
-    MatProgressSpinnerModule
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
